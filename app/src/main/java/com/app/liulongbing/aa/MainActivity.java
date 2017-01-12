e4525802dc5f1158b45657471274ae804955982a
Aleaves <2849459897@qq.com>
2017-01-12 23:15:08 +0800

    master 分支提交

--- app/src/main/java/com/app/liulongbing/aa/MainActivity.java
+++ app/src/main/java/com/app/liulongbing/aa/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //这是dev分支
+        //在master分支 dev分支更改
     }
 }
